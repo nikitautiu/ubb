dd7b7fb00eeceed2f3592fb97697bf7163e452f5
vitiv <nikita.utiu@gmail.com>
2017-05-04 23:01:31 +0300

    SGDB: added directory for SGBD

---
+++ SGBD/lab1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(266, 177);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Incarca";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Insereaza";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(284, 12);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(424, 177);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sterge";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(174, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Actualizeaza";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(324, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(324, 200);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Id";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SGBD/lab1/lab1/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source=DESKTOP-MR3II8A\\SQLEXPRESS;Initial Catalog=Chinook;Integrated Security=True");
+        SqlDataAdapter artistDa = new SqlDataAdapter();
+        DataSet ArtistDs = new DataSet();
+        BindingSource ArtistBs = new BindingSource();
+
+        SqlDataAdapter AlbumDa = new SqlDataAdapter();
+        DataSet AlbumDs = new DataSet();
+        BindingSource AlbumBs = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            artistDa.SelectCommand = new SqlCommand("SELECT * FROM Artist", cs);
+            ArtistDs.Clear();
+            artistDa.Fill(ArtistDs);
+            dataGridView1.DataSource = ArtistDs.Tables[0];
+            ArtistBs.DataSource = ArtistDs.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var cmd = new SqlCommand("INSERT INTO Album VALUES (@AlbumId, @Title, @ArtistId)", cs);
+
+            try
+            {
+
+                var rowInd = dataGridView1.SelectedCells[0].RowIndex;
+                var ArtistId = dataGridView1["ArtistId", rowInd].Value;
+                cmd.Parameters.AddWithValue("@AlbumId", textBox1.Text);
+                cmd.Parameters.AddWithValue("@Title", textBox2.Text);
+                cmd.Parameters.AddWithValue("@Artistid", ArtistId);
+
+
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+
+                AlbumDs.Clear();
+                AlbumDa.Fill(AlbumDs);
+                dataGridView2.DataSource = AlbumDs.Tables[0];
+                AlbumBs.DataSource = AlbumDs.Tables[0];
+                MessageBox.Show("Rand inserat cu succes!");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("A avut loc urmatoarea eroare:" + exc.ToString());
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var cmd = new SqlCommand("UPDATE Album SET Title=@Title WHERE AlbumId=@AlbumId", cs);
+
+            try
+            {
+
+                var rowInd = dataGridView2.SelectedCells[0].RowIndex;
+                var AlbumId = dataGridView2["AlbumId", rowInd].Value;
+                cmd.Parameters.AddWithValue("@AlbumId", AlbumId);
+                cmd.Parameters.AddWithValue("@Title", textBox2.Text);
+
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+
+                AlbumDs.Clear();
+                AlbumDa.Fill(AlbumDs);
+                dataGridView2.DataSource = AlbumDs.Tables[0];
+                AlbumBs.DataSource = AlbumDs.Tables[0];
+                MessageBox.Show("Rand actualizat cu succes!");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("A avut loc urmatoarea eroare:" + exc.ToString());
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count != 0)
+            {
+                AlbumDa.SelectCommand = new SqlCommand("SELECT * FROM Album WHERE ArtistId=@ArtistId", cs);
+                var rowInd = dataGridView1.SelectedCells[0].RowIndex;
+                var ArtistId = dataGridView1["ArtistId", rowInd].Value;
+                AlbumDa.SelectCommand.Parameters.AddWithValue("@ArtistId", ArtistId);
+                AlbumDs.Clear();
+                AlbumDa.Fill(AlbumDs);
+                dataGridView2.DataSource = AlbumDs.Tables[0];
+                AlbumBs.DataSource = AlbumDs.Tables[0];
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // delete
+            if (dataGridView2.SelectedCells.Count != 0)
+            {
+                var cmd = new SqlCommand("DELETE FROM Album WHERE AlbumId=@AlbumId", cs);
+
+                try
+                {
+
+                    var rowInd = dataGridView2.SelectedCells[0].RowIndex;
+                    var AlbumId = dataGridView2["AlbumId", rowInd].Value;
+                    cmd.Parameters.AddWithValue("@AlbumId", AlbumId);
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+
+                    AlbumDs.Clear();
+                    AlbumDa.Fill(AlbumDs);
+                    dataGridView2.DataSource = AlbumDs.Tables[0];
+                    AlbumBs.DataSource = AlbumDs.Tables[0];
+                    MessageBox.Show("Rand sters cu succes!");
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("A avut loc urmatoarea eroare:" + exc.ToString());
+                }
+                finally
+                {
+                    cmd.Connection.Close();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
