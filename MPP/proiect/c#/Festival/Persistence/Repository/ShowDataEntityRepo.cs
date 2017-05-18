56544d73d3357e34bb82bbbe4b44c029c5fc9678
vitiv <nikita.utiu@gmail.com>
2017-05-18 12:13:57 +0300

    MPP: slight modification to the c# project

--- MPP/proiect/c#/Festival/Persistence/Repository/ShowDataEntityRepo.cs
+++ MPP/proiect/c#/Festival/Persistence/Repository/ShowDataEntityRepo.cs
@@ -19,19 +19,21 @@ namespace Persistence.Repository
         {
             using (var db = new DbModel())
             {
-                return (from s in db.Shows
+                var val = (from s in db.Shows
                     join a in db.Artists on s.ArtistId equals a.Id
                     join l in db.Locations on s.LocationId equals l.Id
-                    join p in db.Purchases on s.Id equals p.ShowId
-                    group new {s, a, l, p} by p.ShowId into tick
+                    from p in db.Purchases.Where(p => p.ShowId == s.Id).DefaultIfEmpty()
+                    group new {s, a, l, p} by p.ShowId
+                    into tick
                     select new ShowData
                     {
                         Id = tick.FirstOrDefault().s.Id,
                         ArtistName = tick.FirstOrDefault().a.Name,
                         LocationName = tick.FirstOrDefault().l.Name,
                         StartTime = tick.FirstOrDefault().s.StartTime,
-                        RemainingSeats = tick.Sum(p => p.p.Quanitity)
+                        RemainingSeats = tick.FirstOrDefault().s.AvailableSeats - tick.Sum(p => p.p.Quanitity)
                     }).ToList();
+                return val;
             }
         }
 
