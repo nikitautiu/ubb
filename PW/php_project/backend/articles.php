3a0948ff5a0050940e204f3e9767b9e18e976dc6
vitiv <nikita.utiu@gmail.com>
2017-05-27 18:27:35 +0300

    PW: resturctured directory

--- PW/php_project/backend/articles.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: vitiv
- * Date: 5/21/17
- * Time: 8:20 PM
- */
--- PW/php_project/frontend/js/consts.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by vitiv on 5/23/17.
- */
-var apiEndpoint = '/api';
-var useAPI = false;
