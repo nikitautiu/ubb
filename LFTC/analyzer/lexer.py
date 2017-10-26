84e918779012f2465939025bc52d4308d154e4a1
Nichita Utiu <nikita.utiu@gmail.com>
2017-10-26 23:13:25 +0300

    added partial analyzer commit

---
+++ LFTC/analyzer/lexer.py
@@ -0,0 +1,123 @@
+import re
+from collections import namedtuple
+
+# the type to hold the definition of a symbol
+SymbolDefinition = namedtuple('SymbolDefinition', ['code', 'id', 'regex'])
+
+
+def symbol_def_from_list(def_tuples):
+    """Given a list of tuples with
+    (code, id, regex) generate a list of symbol definitions
+    from it"""
+    return [SymbolDefinition(code, id, re.compile(r'({}).*'.format(regex))) for code, id, regex in def_tuples]
+
+
+class ParseError(Exception):
+    def __init__(self, line, pos, line_text):
+        error_msg = "A parse error occurred on line {}, position {}: \n {}".format(line, pos, line_text)
+        super().__init__(error_msg)
+
+
+class SymbolTable(object):
+    def __init__(self):
+        self.symbols = []
+
+    def get(self, hash):
+        return self.symbols[hash]
+
+    def put(self, symbol):
+        if symbol not in self.symbols:
+            self.symbols.append(symbol)
+        return self.symbols.index(symbol)
+
+
+class BaseLexer(object):
+    """Base class for lexing
+    Initialized with definitions for the following categories of symbols:
+    * separators
+    * reserved words
+    * operators
+    * identifiers
+    * constants
+    The definitions are given as lists of `SymbolDefinition`.
+    """
+
+    def __init__(self, separators, reserved, operators, identifiers, constants):
+        # intialize the lists of definitions
+        self.constants = symbol_def_from_list(constants)
+        self.identifiers = symbol_def_from_list(identifiers)
+        self.operators = symbol_def_from_list(operators)
+        self.reserved = symbol_def_from_list(reserved)
+        self.separators = symbol_def_from_list(separators)
+
+    def parse_with_definitions(self, text, def_list):
+        """Given a text, and a list of definitions try to return the definition
+        and the full text of the match, if any. If none is matched, return `None`."""
+        for definition in def_list:
+            match_obj = definition.regex.match(text)
+            if match_obj:
+                # if there is a match return it
+                return definition, match_obj.group(1)
+            # otherwise just go on
+
+        # if none is found, return none
+        return None
+
+    def parse_text(self, text):
+        """Passes over all the definitions in this order
+        * separators
+        * reserved words
+        * constants
+        * identifiers
+        * operators
+        And returns the definition and matching text"""
+        for def_list in [self.separators, self.reserved, self.constants,
+                         self.identifiers, self.operators]:
+            match = self.parse_with_definitions(text, def_list)
+            if match:
+                if def_list is self.identifiers and len(match[1]) > 8:
+                    # check that identifiers are shorter than 8 characters
+                    raise ParseError("Identifier {} longer than 8 characters".format(match[1]))
+                return match  # return it if it exists
+
+        return None
+
+    def parse(self, text):
+        """Receives a text, parses it and returns it internal form and
+        the symbol tables corresponding to the identifiers and constants.
+
+        The return value is a tuple of (internal_form, identifier_table, constant_table)"""
+        pos = 0
+        internal_form = []
+
+        # initialize the symbol tables
+        constant_table = SymbolTable()
+        identifier_table = SymbolTable()
+
+        # iterate over the string, token by token
+        while pos != len(text):
+            match = self.parse_text(text[pos:])  # only pos and above
+            if match:
+                match_def, match_text = match  # unpack the tuple
+
+                index = -1
+                # add it to either the constants or identifiers if needed
+                if match_def in self.identifiers:
+                    index = identifier_table.put(match_text)
+                if match_def in self.constants:
+                    index = constant_table.put(match_text)
+
+                # index will be -1 if neither of them
+                internal_form.append((match_def.code, index))  # add the token
+
+                # increment the position
+                pos += len(match_text)
+            else:
+                # return the rest of the line as the error text
+                line_num = text[:pos].count('\n')
+                line_pos = pos - text.rfind('\n', 0, pos)  # find position on line
+                line_text = text[pos:text.find('\n', pos)]
+                raise ParseError(line_num, line_pos, line_text)  # return the verbose error
+
+        # return the result
+        return internal_form, identifier_table, constant_table
---
+++ LFTC/analyzer/tests/test_baseLexer.py
@@ -0,0 +1,76 @@
+from unittest import TestCase
+
+from lexer import BaseLexer, ParseError
+
+
+def remove_space_tokens(token_list):
+    return list(filter(lambda x: x != 5, (fip[0] for fip in token_list)))
+
+
+class TestBaseLexer(TestCase):
+    def setUp(self):
+        identifiers = [
+            (0, 'IDENTIFIER', r'[a-z][a-z0-9]*')
+        ]
+        constants = [
+            (1, 'CONSTANT', r'(\+|-)?[1-9][0-9]*|0')
+        ]
+        separators = [
+            (2, '(', r'\('),
+            (3, ')', r'\)'),
+            (4, ';', r';+'),
+            (5, ' ', r'\s+')
+        ]
+        reserved = [
+            (6, 'signed', r'signed')
+        ]
+        operators = [
+            (7, '<=', r'<='),
+            (8, '>=', r'>='),
+            (9, '<', r'<'),
+            (10, '>', r'>'),
+            (11, '+', r'\+'),
+            (12, '-', r'-'),
+            (13, '*', r'\*'),
+            (14, '/', r'/')
+        ]
+        # initialize te lexer
+        self.lexer = BaseLexer(separators, reserved, operators, identifiers, constants)
+
+    def test_internal_form(self):
+        # valid example, test if properly tokenized
+        test_string = '1  + 2 - -3 * signed vari <= 15 > 0; 12'
+        expected_codes = [
+            1,   # 1
+            11,  # +
+            1,   # 2
+            12,  # -
+            1,   # -3
+            13,  # *
+            6,   # signed
+            0,   # vari
+            7,   # <=
+            1,   # 15
+            10,  # >
+            1,   # 0
+            4,   # ;
+            1    # 12
+        ]
+
+        internal_form, _, _ = self.lexer.parse(test_string)
+        code_list = remove_space_tokens(internal_form)  # remove all spaces
+        self.assertListEqual(code_list, expected_codes)
+
+        # invalid example, invalid operator ^
+        test_string = '1  + 2 - -3 * signed vari <= 15 > 0; ^ 12 /  a + b'
+        with self.assertRaises(ParseError):
+            self.lexer.parse(test_string)
+
+        # valid multiline test
+        test_string = '1  + 2 - -3 * signed vari <= 15 > 0; 12\n  a + b ;; a'
+        internal_form, _, _ = self.lexer.parse(test_string)
+
+        # invalid example, invalid operator ^
+        test_string = '1  + 2 - -3 * signed vari <= 15 > 0; 12 /  \n a + ^ b'
+        with self.assertRaises(ParseError):
+            self.lexer.parse(test_string)
