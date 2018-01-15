58087ac0abc2e889c11b0b093c9c4fd1c3cfe7ea
Nichita Utiu <nikita.utiu@gmail.com>
2018-01-15 14:36:12 +0200

    added parser for lab 5 LFTC

--- LFTC/lab5/parser/parser.py
+++ LFTC/lab5/parser/parser.py
@@ -19,6 +19,7 @@ IDENTIFIER: '[_a-zA-Z]+[_0-9a-zA-Z]*'
 
 CONST: '([-+]?(([1-9][0-9]*)|0)(?![0-9]))|([-+]?(([1-9][0-9]*)|(0?))(\.[0-9]+)(?![0-9]))|("[^"]*")' ;
 SPACES: '[ \t\n]+' (%ignore) ; 
+OPASSIGN: '\+=|-=|\*=|/=|\*=|&&=|\|\|=|\^=|=';
 
 program: func_declaration_list ;
 func_declaration_list: func_declaration | func_declaration+ func_declaration_list ;
@@ -26,10 +27,11 @@ func_declaration: func_header instruction_block ;
 func_header: func_return '\(' '\)' | func_return '\(' func_signature '\)' ;
 func_return: type IDENTIFIER | VOID IDENTIFIER  ;
 @func_signature:  func_param_declaration | (func_param_declaration ',')+ func_signature ;
-func_param_declaration: type IDENTIFIER ;
+func_param_declaration: type param ;
+param: IDENTIFIER;
 instruction_block: '\{' '\}' | '\{' instruction_list '\}' ;
 @instruction_list: instruction | instruction+ instruction_list ;
-@instruction:  conditional_instr | loop_instr | return_instr | expr_instr | declaration_instr | assignment_instr;
+@instruction:  conditional_instr | loop_instr | return_instr | call_instr | declaration_instr | assignment_instr;
 declaration_instr: type assignment_expr_list';' ;
 @assignment_expr_list: assignment_expr | (assignment_expr ',')+ assignment_expr_list ;
 assignment_instr: assignment_expr_list ';' ;
@@ -37,36 +39,43 @@ type: FLOAT | INT | BOOL | CHAR | FLOAT array_type | INT array_type | BOOL array
 array_type: array_size | array_size array_type ;
 array_size: '\[' CONST '\]'  ;
 @conditional_instr: if_instr elif_instr | if_instr elif_instr_list | if_instr elif_instr else_instr | if_instr elif_instr_list else_instr ;
-if_instr: IF '\(' expr '\)' instruction_block ;
+if_instr: IF '\(' condition '\)' instruction_block ;
 elif_instr_list: elif_instr | elif_instr elif_instr_list ;
-elif_instr: IF ELSE '\(' expr '\)' instruction_block ;
-else_instr: ELSE '\(' expr '\)' instruction_block ;
-loop_instr: WHILE '\(' expr '\)' instruction_block ;
-return_instr: RETURN expr ';' ;
-expr_instr: expr ';' ;
-@expr: assignment_expr | binary_expr | pre_unary_expr | index_expr | call_expr | CONST | IDENTIFIER ;
-assignment_expr: IDENTIFIER '=' expr ;
-binary_expr: expr binary_op expr ;
+elif_instr: IF ELSE '\(' condition '\)' instruction_block ;
+else_instr: ELSE '\(' condition '\)' instruction_block ;
+loop_instr: WHILE '\(' condition '\)' instruction_block ;
+condition: expr;
+return_instr: RETURN ret_value ';' ;
+ret_value: expr;
+call_instr: call_expr ';' ;
+@expr: binary_expr | pre_unary_expr | index_expr | call_expr | CONST | IDENTIFIER ;
+assignment_expr: lvalue assign_op rvalue ;
+assign_op: OPASSIGN ;
+lvalue: IDENTIFIER ;
+rvalue: expr;
+binary_expr: term binary_op term ;
+term: expr;
 binary_op: '<' | '>' | '<=' | '>=' | '!=' | '==' | '\+' | '-' | '/' | '\*' | '&&' | '\|\|' | '\^' ;
 index_expr: IDENTIFIER subscript_list ;
-subscript_list: subscript | subscript subscript_list ;
+subscript_list: subscript | subscript+ subscript_list ;
 subscript: '\[' expr '\]' ;
-pre_unary_expr: pre_unary_op expr ;
+pre_unary_expr: pre_unary_op term ;
 pre_unary_op: '\+' | '-' | '!' ;
 call_expr: IDENTIFIER '\(' expr_list '\)' ;
-@expr_list: expr | (expr ',')+ expr_list ;
+@expr_list: param | (param ',')+ expr_list ;
+param: expr;
 """)
 
 res = prog_parser.parse("""
 int print(int a, int b) {
-    int x = 10, y = a;
+    int x = 10, y = a + 2;
     while(x < a) {
-        x = x + y;
-        y = a;
+        x = x - y;
+        y += a;
     }
     x(1, 2, 1);
     return 7;
 }
 """)
 
-print(res)
+res.to_png_with_pydot('example.png')
